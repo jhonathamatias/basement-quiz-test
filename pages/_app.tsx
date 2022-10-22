3f1ed53107ca8b0c88ff0bb999453f82831e8ec1
jhonathamatias <jhonathamatias35@gmail.com>
2022-10-22 10:48:35 -0300

    Created api context to manage application state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GameProvider } from '../src/store/GameContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <GameProvider>
+    <Component {...pageProps} />
+  </GameProvider>
 }
 
 export default MyApp
